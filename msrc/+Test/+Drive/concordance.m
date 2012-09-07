ca9559e8c0a453d1fd1f334f1a43eae9c0206e50
Ivan Ukhov <uvsoft@gmail.com>
2012-09-07 09:26:21 +0200

    Add +Test/+Drive/concordance.m

---
+++ msrc/+Test/+Drive/concordance.m
@@ -0,0 +1,34 @@
+clear all;
+clc;
+
+Nrv = 4;
+Nmcs = 1e5;
+
+%% Generate a Pearson's correlation matrix.
+%
+S = normrnd(0, 1, Nrv, Nrv);
+S = S' * S;
+Cp = corrcov(S);
+
+%% Convert it to the corresponding Spearman's one.
+%
+Cs = asin(Cp / 2) * 6 / pi;
+
+%% Correlate the RVs.
+%
+Y = mvnrnd(zeros(Nmcs, Nrv), Cp);
+CsY = corr(Y, 'type', 'Spearman');
+
+%% Transform the correlated normal RVs into uniform ones.
+%
+U = normcdf(Y);
+CsU = corr(U, 'type', 'Spearman');
+
+%% Transform the uniform RVs to, say, exponential RVs.
+%
+X = expinv(U);
+CsX = corr(X, 'type', 'Spearman');
+
+fprintf('Infinity norm for Y: %e\n', norm(Cs - CsY, Inf));
+fprintf('Infinity norm for U: %e\n', norm(Cs - CsU, Inf));
+fprintf('Infinity norm for X: %e\n', norm(Cs - CsX, Inf));
