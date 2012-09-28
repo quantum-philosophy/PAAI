ea98bc4d9865c108e32e2b560dda0190e583a157
Ivan Ukhov <uvsoft@gmail.com>
2012-09-28 18:36:28 +0200

    Add vendor/spinterp

--- msrc/init.m
+++ msrc/init.m
@@ -1,5 +1,5 @@
 clear all;
 
 path(path, [ Constants.vendorDirectory, '/', 'SPARSE_GRID_HW' ]);
-path(path, [ Constants.vendorDirectory, '/', 'SPARSE_GRID_HW' ]);
+path(path, [ Constants.vendorDirectory, '/', 'spinterp' ]);
 path(path, [ Constants.vendorDirectory, '/', 'sympoly' ]);
