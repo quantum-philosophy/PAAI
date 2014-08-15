0f8f1a846eb189bdfe46a3db7df16227db2a20fc
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-15 10:36:11 +0200

    Recovered +Test/showTemperature

--- src/+Test/showTemperature.m
+++ src/+Test/showTemperature.m
@@ -1,40 +1,48 @@
-setup;
+function showTemperature()
+  setup;
 
-samplingInterval = 1e-4; % s
-processorCount = 2;
-taskCount = 20;
+  samplingInterval = 1e-4; % s
+  processorCount = 2;
+  taskCount = 20;
 
-%% Configuration.
-%
-[ platform, application ] = parseTGFF(Utils.resolvePath( ...
-sprintf('%03d_%03d.tgff', processorCount, taskCount)));
+  %% Configuration.
+  %
+  [platform, application] = Utils.parseTGFF(Utils.resolvePath( ...
+  sprintf('%03d_%03d.tgff', processorCount, taskCount)));
 
-floorplan = Utils.resolvePath( ...
-sprintf('%03d.flp', processorCount), 'test');
-hotspotConfig = Utils.resolvePath( ...
-  'hotspot.config', 'test');
-hotspotLine = sprintf('sampling_intvl %e', ...
-  samplingInterval);
+  floorplan = Utils.resolvePath( ...
+    sprintf('%03d.flp', processorCount), 'test');
+  hotspotConfig = Utils.resolvePath( ...
+    'hotspot.config', 'test');
+  hotspotLine = sprintf('sampling_intvl %e', ...
+    samplingInterval);
 
-%% Schedule the application.
-%
-schedule = Schedule.Dense(platform, application);
+  %% Schedule the application.
+  %
+  scheduler = Scheduler.Dense('platform', platform, 'application', application);
+  schedule = scheduler.compute();
 
-plot(schedule);
-display(schedule);
+  scheduler.plot(schedule);
+  scheduler.display(schedule);
 
-%% Obtain the dynamic power profile.
-%
-power = DynamicPower(samplingInterval);
-powerProfile = power.compute(schedule);
+  %% Obtain the dynamic power profile.
+  %
+  power = DynamicPower('platform', platform, 'application', application, ...
+    'samplingInterval', samplingInterval);
+  powerProfile = power.compute(schedule);
 
-power.display(powerProfile);
-power.plot(powerProfile);
+  power.display(powerProfile);
+  power.plot(powerProfile);
 
-%% Compute the corresponding temperature profile.
-%
-hotspot = HotSpot.Analytic(floorplan, hotspotConfig, hotspotLine);
-temperatureProfile = hotspot.compute(powerProfile);
+  %% Compute the corresponding temperature profile.
+  %
+  temperature = Temperature.Analytical.Transient( ...
+    'floorplan', floorplan, ...
+    'hotspotConfig', hotspotConfig, ...
+    'hotspotLine', hotspotLine, ...
+    'samplingInterval', samplingInterval, ...
+    'processorCount', processorCount);
 
-display(hotspot);
-hotspot.plot(temperatureProfile);
+  temperatureProfile = temperature.compute(powerProfile);
+  temperature.plot(temperatureProfile);
+end
