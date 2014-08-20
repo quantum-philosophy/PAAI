a3244115ddce4ec19b6d67113a6de931366c3302
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-20 16:04:54 +0200

    Moved the TA configuration into Test/Config/temperature

--- src/+Test/showTemperature.m
+++ src/+Test/showTemperature.m
@@ -1,7 +1,7 @@
 function showTemperature()
   setup;
 
-  samplingInterval = 1e-3; % s
+  samplingInterval = 1e-5; % s
   processorCount = 2;
   taskCount = 20;
 
@@ -9,30 +9,6 @@ function showTemperature()
   %
   [platform, application] = Utils.parseTGFF(Utils.resolvePath( ...
     sprintf('%03d_%03d.tgff', processorCount, taskCount)));
-
-  floorplan = Utils.resolvePath(sprintf('%03d.flp', processorCount));
-  hotspotConfig = Utils.resolvePath('hotspot.config');
-  hotspotLine = [ ...
-    sprintf(' t_chip %e', 0.00015), ... 0.00015
-    sprintf(' k_chip %e', 100.0  ), ... 100.0
-    sprintf(' p_chip %e', 1.75e6 ), ... 1.75e6
-    ...
-    sprintf(' t_interface %e', 2.0e-05), ... 2.0e-05
-    sprintf(' k_interface %e', 4.0    ), ... 4.0
-    sprintf(' p_interface %e', 4.0e6  ), ... 4.0e6
-    ...
-    sprintf(' s_sink %e', 0.06  ), ... 0.06
-    sprintf(' t_sink %e', 0.0069), ... 0.0069
-    sprintf(' k_sink %e', 400.0 ), ... 400.0
-    sprintf(' p_sink %e', 3.55e6), ... 3.55e6
-    ...
-    sprintf(' s_spreader %e', 0.03  ), ... 0.03
-    sprintf(' t_spreader %e', 0.001 ), ... 0.001
-    sprintf(' k_spreader %e', 400.0 ), ... 400.0
-    sprintf(' p_spreader %e', 3.55e6), ... 3.55e6
-    ...
-    sprintf(' sampling_intvl %e', samplingInterval) ...
-  ];
 
   %% Schedule the application.
   %
@@ -53,10 +29,7 @@ function showTemperature()
 
   %% Compute the corresponding temperature profile.
   %
-  temperature = Temperature.Analytical.Transient( ...
-    'floorplan', floorplan, ...
-    'hotspotConfig', hotspotConfig, ...
-    'hotspotLine', hotspotLine, ...
+  temperature = Test.Configure.temperature( ...
     'samplingInterval', samplingInterval, ...
     'processorCount', processorCount);
 
