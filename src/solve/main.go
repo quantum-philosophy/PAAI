23ff08c0c12d16d826540c78c23cb1bae9f02d9b
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-12 14:44:30 +0200

    Added the possibility to write the result in a MAT file

--- src/solve/main.go
+++ src/solve/main.go
@@ -4,9 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"time"
+
+	"github.com/go-math/format/mat"
 )
 
-var config = flag.String("config", "", "the configuration file in JSON")
+var config = flag.String("config", "", "a configuratin file in JSON")
+var output = flag.String("output", "", "an output file in MAT")
 
 func printUsage() {
 	fmt.Printf("Usage: solve [options]\nOptions:\n")
@@ -39,4 +42,21 @@ func main() {
 
 	fmt.Println(surrogate)
 	fmt.Println("Time elapsed:", finish.Sub(start))
+
+	if len(*output) == 0 {
+		return
+	}
+
+	file, err := mat.Open(*output, "w7.3")
+	if err != nil {
+		printError(err)
+		return
+	}
+	defer file.Close()
+
+	err = file.Put("surrogate", *surrogate)
+	if err != nil {
+		printError(err)
+		return
+	}
 }
