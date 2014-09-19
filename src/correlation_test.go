d7a5b4b903e3d23f566fa4c1ae13d115c3219e83
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-19 17:07:13 +0200

    Added a test for ascend

--- src/correlation_test.go
+++ src/correlation_test.go
@@ -4,7 +4,20 @@ import (
 	"testing"
 
 	"github.com/go-eslab/persim/system"
+	"github.com/go-math/support/assert"
 )
+
+func TestAscend(t *testing.T) {
+	_, app, _ := system.Load("fixtures/002_020.tgff")
+
+	assert.Equal(ascend(app, 0, 0), 0.0, t)
+	assert.Equal(ascend(app, 1, 0), 1.0, t)
+	assert.Equal(ascend(app, 2, 0), 2.0, t)
+	assert.Equal(ascend(app, 13, 0), 4.0, t)
+	assert.Equal(ascend(app, 18, 9), (2.0+2.0)/2.0, t)
+	assert.Equal(ascend(app, 19, 0), (2.0+5.0)/2.0, t)
+	assert.Equal(ascend(app, 0, 19), -1.0, t)
+}
 
 func BenchmarkCorrelate(b *testing.B) {
 	_, app, _ := system.Load("fixtures/002_020.tgff")
